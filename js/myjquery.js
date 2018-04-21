57bd64f7e27c6534c107b6a8c5909ac2a39cc54b
Mohammad <m2back.3d@gmail.com>
2018-04-21 10:03:10 +0430

    first commit

---
+++ js/myjquery.js
@@ -0,0 +1,28 @@
+$(window).scroll(function(){
+        var scroll_top=$(this).scrollTop();
+
+        if (scroll_top>300){
+                $("#to_top").animate({right:"10px"},50);
+        }
+        else{
+                $("#to_top").animate({right:"-100px"},50);
+        }
+});
+
+
+$(document).ready(function(){
+        $("#to_top").click(function(){
+                $("html").animate({scrollTop:0},1000);
+        });
+
+
+});
+
+
+$(window).scroll(function(){
+    var sticky = $('.sticky'),
+            scroll = $(window).scrollTop();
+
+    if (scroll >= 200) sticky.addClass('fixed');
+    else sticky.removeClass('fixed');
+});
